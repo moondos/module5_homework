ac4d6485ce4444fe2ebb38824799436eff58cb96
Aidos Kenessov <47719645+moondos@users.noreply.github.com>
2021-02-20 16:03:26 +0000

    Create Task 2.js

---
+++ Task 2.js
@@ -0,0 +1,15 @@
+const x = "Hello";
+
+switch (typeof x) {
+    case "number":
+        console.log("X — число");
+        break;
+    case "string":
+        console.log("X — строка");
+        break;
+    case "boolean":
+        console.log("X — логический тип");
+        break;
+    default:
+        console.log("Тип x не определён");
+}
